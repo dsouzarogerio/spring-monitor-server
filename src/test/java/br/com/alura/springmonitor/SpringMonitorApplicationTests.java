f474de98edc8a3828783d001e5bb0658afec081c
Rogério de Souza <dsouza.rogerio@gmail.com>
2023-01-17 19:58:17 -0300

    configuracao spring-boot-admin-server

---
+++ src/test/java/br/com/alura/springmonitor/SpringMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.springmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
